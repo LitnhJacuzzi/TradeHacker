2db5906d147df2b318390baaeb29a5072676d96d
LitnhJacuzzi <2075397019@qq.com>
2024-10-14 12:47:37 +0800

    Push for commit.

---
+++ src/main/java/org/litnhjacuzzi/tradehacker/TradeHacker.java
@@ -0,0 +1,9 @@
+package org.litnhjacuzzi.tradehacker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TradeHacker implements ModInitializer 
+{
+	@Override
+	public void onInitialize() {}
+}
