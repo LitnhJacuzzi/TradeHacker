c4cc6b8d31b5c501bfcd79729400b4b5a353ec56
LitnhJacuzzi <2075397019@qq.com>
2025-10-03 14:23:32 +0800

    Extend supported versions.

--- src/main/java/org/litnhjacuzzi/tradehacker/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/org/litnhjacuzzi/tradehacker/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,5 +1,6 @@
 package org.litnhjacuzzi.tradehacker.mixin;
 
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,13 +8,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.SelectMerchantTradeC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.village.Merchant;
+import net.minecraft.world.GameMode;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
@@ -24,17 +25,15 @@ public class ServerPlayNetworkHandlerMixin {
 	@Inject(method = {"onMerchantTradeSelect", "onSelectMerchantTrade"}, at = @At(value = "INVOKE", target = 
 			"Lnet/minecraft/screen/MerchantScreenHandler;switchTo(I)V"))
 	public void getItemIfCreative(SelectMerchantTradeC2SPacket packet, CallbackInfo ci) {
-		if(player.isCreative()) {
-			boolean isShiftPressed = InputUtil.isKeyPressed(
-					MinecraftClient.getInstance().getWindow().getHandle(), 340);
-			hackItem(player, packet.getTradeId(), isShiftPressed);
+		if(player.interactionManager.getGameMode() == GameMode.CREATIVE) {
+			hackItem(player, packet.getTradeId());
 		}
 	}
 	
-	private void hackItem(PlayerEntity player, int selectedIndex, boolean isShiftPressed) {
+	private void hackItem(PlayerEntity player, int selectedIndex) {
 		Merchant merchant = (Merchant) ((MerchantScreenHandlerMixin) player.currentScreenHandler).getMerchant();
 		ItemStack targetItem = merchant.getOffers().get(selectedIndex).copySellItem();
-		if(isShiftPressed) {
+		if(GLFW.glfwGetKey(MinecraftClient.getInstance().getWindow().getHandle(), 340) == 1) {
 			targetItem.setCount(targetItem.getMaxCount());
 		}
 		player.getInventory().insertStack(targetItem);
